fae6376bf31964462bb61f04fccb0c9cac966a2d
姜峰 <jiang.f@Ctrip.com>
2017-08-18 10:04:39 +0800

    save

--- src/main.js
+++ src/main.js
@@ -1,12 +1,26 @@
 import Vue from 'vue'
+import Vuex from 'vuex'
 import App from './App.vue'
 import router from './routes.js'
 
 Vue.config.debug = true;//开启错误提示
 
+Vue.use(Vuex)
+const store = new Vuex.Store({
+  state: {
+    count: 0
+  },
+  mutations: {
+    increment (state) {
+      state.count++
+    }
+  }
+})
+
 new Vue({
   el: '#app',
   router,
+  store,
   template: '<App />',
   components: {App}
 })
--- src/views/about.vue
+++ src/views/about.vue
@@ -1,3 +1,33 @@
 <template>
-    <div>about</div>
+    <div>
+        <div>count: {{count}}</div>
+        <button @click="add">add</button>
+        <button @click="reduce">reduce</button>
+    </div>
 </template>
+<script>
+import { mapState } from 'vuex'
+export default {
+    // data(){
+    //     return {
+    //         count: 0
+    //     }
+    // },
+    // computed: {
+    //     count(){
+    //         return this.$store.state.count
+    //     }
+    // },
+    computed: mapState({
+        count: state => state.count
+    }),
+    methods:{
+        add() {
+            this.$store.commit('increment')
+        },
+        reduce() {
+            // this.count--
+        }
+    }
+}
+</script>
